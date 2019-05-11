6c070aaf91378d14aa89030712cfd1927f0966bc
Komi Amiko <komiamiko@gmail.com>
2019-05-10 20:19:23 -0400

    switched to serenity with dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,56 @@
-extern crate blursed;
+use dotenv;
 
-use discord::Discord;
-use discord::model::Event;
-use std::env;
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    // Set a handler for the `message` event - so that whenever a new message
+    // is received - the closure (or function) passed will be called.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple
+    // events can be dispatched simultaneously.
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "b,ping" {
+            // Sending a message can fail, due to a network error, an
+            // authentication error, or lack of permissions to post in the
+            // channel, so log to stdout when some error happens, with a
+            // description of it.
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a
+    // shard is booted, and a READY payload is sent by Discord. This payload
+    // contains data like the current user's guild Ids, current user data,
+    // private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 
 fn main() {
-	// Log in to Discord using a bot token from the environment
-	let discord = Discord::from_bot_token(
-		&env::var("TOKEN_DISCORD").expect("Expected token"),
-	).expect("login failed");
+    // Configure the client with your Discord bot token in the environment.
+    let token = String::from(dotenv::var("TOKEN_DISCORD")
+        .expect("Expected a token in the environment"));
 
-	// Establish and use a websocket connection
-	let (mut connection, _) = discord.connect().expect("connect failed");
-	println!("Ready.");
-	loop {
-		match connection.recv_event() {
-			Ok(Event::MessageCreate(message)) => {
-				println!("{} says: {}", message.author.name, message.content);
-				if message.content == "b,test" {
-					let _ = discord.send_message(message.channel_id, "This is a reply to the test.", "", false);
-				} else if message.content == "b,quit" {
-					println!("Quitting.");
-					break
-				}
-			}
-			Ok(_) => {}
-			Err(discord::Error::Closed(code, body)) => {
-				println!("Gateway closed on us with code {:?}: {}", code, body);
-				break
-			}
-			Err(err) => println!("Receive error: {:?}", err)
-		}
-	}
+    // Create a new instance of the Client, logging in as a bot. This will
+    // automatically prepend your bot token with "Bot ", which is a requirement
+    // by Discord for bot users.
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start() {
+        println!("Client error: {:?}", why);
+    }
 }
