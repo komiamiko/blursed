fe5ec19c2e3854658ad9174664c6c827d4d511cb
Nova <nova185@gmx.com>
2019-06-07 09:47:56 -0400

    added string parsing literals

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,13 @@
-use nom::{named, escaped_transform, map, alt, call, tag};
+use nom::{named, escaped_transform, map, alt, call, tag, alpha};
 
 
-named!(escape,
+fn to_s(i: Vec<u8>) -> String {
+    String::from_utf8_lossy(&i).into_owned()
+}
+
+named!(escape < String >,
     map!(
-        escaped_transform!(call!(alpha1), '\\',
+        escaped_transform!(call!(alpha), '\\',
             alt!(
                 tag!("\\")  => { |_| &b"\\"[..] } |
                 tag!("\"")  => { |_| &b"\""[..] } |
@@ -11,17 +15,34 @@ named!(escape,
                 tag!("t")   => { |_| &b"\t"[..] } |
                 tag!("r")   => { |_| &b"\r"[..] }
             )
-        )
+        ), to_s
     )
 );
 
-pub fn parse_string_literal(s: &str) -> String {
+pub fn parse_string_literals(s: &str) -> String {
     let bytes = s.as_bytes();
+    let out = escape(bytes).unwrap();
+    out.1
 }
 
+#[cfg(test)]
+mod tests {
+    use super::parse_string_literals;
 
-#[test]
-fn basic_literal_test() {
+    const LITERAL_MAPS: &[(&str, &str)] = &[
+        (r#"\n"#, "\n"),
+        (r#"\\"#, "\\"),
+        (r#"\t"#, "\t"),
+        (r#"\r"#, "\r"),
+    ];
 
+    #[test]
+    fn basic_literal_test() {
+        LITERAL_MAPS.iter().for_each(|(r, p)|{
+            let out = parse_string_literals(*r);
+            let out_raw = out.as_str();
+            assert_eq!(out_raw, *p);
+        });
+    }
 }
 
